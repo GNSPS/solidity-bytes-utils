0501e89b0c8ffae526e41a5954b9897761f04e6c
Niran Babalola <niran@niran.org>
2018-01-04 02:21:35 -0600

    Add comments to concatStorage

--- contracts/BytesLib.sol
+++ contracts/BytesLib.sol
@@ -75,8 +75,17 @@ library BytesLib {
 
     function concatStorage(bytes storage _preBytes, bytes memory _postBytes) internal {
         assembly {
-            // we know _preBytes_offset is 0
+            // Read the first 32 bytes of _preBytes storage, which is the length
+            // of the array. (We don't need to use the offset into the slot
+            // because arrays use the entire slot.)
             let fslot := sload(_preBytes_slot)
+            // Arrays of 31 bytes or less have an even value in their slot,
+            // while longer arrays have an odd value. The actual length is
+            // the slot divided by two for odd values, and the lowest order
+            // byte divided by two for even values.
+            // If the slot is even, bitwise and the slot with 255 and divide by
+            // two to get the length. If the slot is odd, bitwise and the slot
+            // with -1 and divide by two.
             let slength := div(and(fslot, sub(mul(0x100, iszero(and(fslot, 1))), 1)), 2)
             let mlength := mload(_postBytes)
             let newlength := add(slength, mlength)
@@ -85,6 +94,8 @@ library BytesLib {
             // v. http://solidity.readthedocs.io/en/latest/miscellaneous.html#layout-of-state-variables-in-storage
             switch add(lt(slength, 32), lt(newlength, 32))
             case 2 {
+                // Since the new array still fits in the slot, we just need to
+                // update the contents of the slot.
                 // uint256(bytes_storage) = uint256(bytes_storage) + uint256(bytes_memory) + new_length
                 sstore(
                     _preBytes_slot,
@@ -114,6 +125,8 @@ library BytesLib {
                 )
             }
             case 1 {
+                // The stored value fits in the slot, but the combined value
+                // will exceed it.
                 // get the keccak hash to get the contents of the array
                 mstore(0x0, _preBytes_slot)
                 let sc := add(keccak256(0x0, 0x20), div(slength, 32))
@@ -121,10 +134,24 @@ library BytesLib {
                 // save new length
                 sstore(_preBytes_slot, add(mul(newlength, 2), 1))
 
+                // The contents of the _postBytes array start 32 bytes into
+                // the structure. Our first read should obtain the `submod`
+                // bytes that can fit into the unused space in the last word
+                // of the stored array. To get this, we read 32 bytes starting
+                // from `submod`, so the data we read overlaps with the array
+                // contents by `submod` bytes. Masking the lowest-order
+                // `submod` bytes allows us to add that value directly to the
+                // stored value.
+
                 let submod := sub(32, slength)
                 let mc := add(_postBytes, submod)
                 let end := add(add(_postBytes, 0x20), mlength)
                 let mask := sub(exp(0x100, submod), 1)
+
+                // FIXME: I think using add() to combine stored data with the
+                // concatenated bytes only works for arrays that have never
+                // been shortened by setting bytes.length, which will leave
+                // junk in the lowest-order bytes.
 
                 sstore(
                     sc,
@@ -150,17 +177,23 @@ library BytesLib {
             default {
                 // get the keccak hash to get the contents of the array
                 mstore(0x0, _preBytes_slot)
+                // Start copying to the last used word of the stored array.
                 let sc := add(keccak256(0x0, 0x20), div(slength, 32))
 
                 // save new length
                 sstore(_preBytes_slot, add(mul(newlength, 2), 1))
 
+                // Copy over the first `submod` bytes of the new data as in
+                // case 1 above.
                 let slengthmod := mod(slength, 32)
                 let submod := sub(32, slengthmod)
                 let mc := add(_postBytes, submod)
                 let end := add(mc, mlength)
                 let mask := sub(exp(0x100, submod), 1)
 
+                // FIXME: I think this only works for arrays that have never
+                // been shortened by setting bytes.length, which will leave
+                // junk in the lowest-order bytes.
                 sstore(sc, add(sload(sc), and(mload(mc), mask)))
 
                 for {
