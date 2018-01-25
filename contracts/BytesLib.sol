fb73c34ac97a1bc6960af08212c71243fb18bfb2
Gonçalo Sá <goncalo05@gmail.com>
2018-01-25 02:02:36 +0000

    Add type cast method to `bytes32`

--- contracts/BytesLib.sol
+++ contracts/BytesLib.sol
@@ -297,6 +297,17 @@ library BytesLib {
         return tempUint;
     }
 
+    function toBytes32(bytes _bytes, uint _start) internal  pure returns (bytes32) {
+        require(_bytes.length >= (_start + 32));
+        bytes32 tempBytes32;
+
+        assembly {
+            tempBytes32 := mload(add(add(_bytes, 0x20), _start))
+        }
+
+        return tempBytes32;
+    }
+
     function equal(bytes memory _preBytes, bytes memory _postBytes) internal pure returns (bool) {
         bool success = true;
 
