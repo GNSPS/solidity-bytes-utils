77274204c7858beaa84d72031c9c9e866f03c3cf
Gonçalo Sá <goncalo05@gmail.com>
2020-01-09 00:26:54 +0000

    Change truffle config file to always use the right compielr version

--- truffle.js
+++ truffle.js
@@ -40,5 +40,10 @@ module.exports = {
     //   port: 7545,
     //   network_id: "*" // Match any network id
     // },
+  },
+  compilers: {
+    solc: {
+      version: "0.5.14", // A version or constraint - Ex. "^0.5.0"
+    }
   }
 };
